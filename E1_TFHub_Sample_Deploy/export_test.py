de1f7ddb53257e5c8b7891211a9f37a7bae479dc
captain-pool <rickdey1998@gmail.com>
2019-06-17 21:46:31 +0530

    Fixed unittests

--- E1_TFHub_Sample_Deploy/export_test.py
+++ E1_TFHub_Sample_Deploy/export_test.py
@@ -1,50 +1,51 @@
 import tensorflow as tf
-import numpy as np
-import tensorflow_datasets as tfds
 import tensorflow_hub as hub
 import unittest
-import argparse
+import tempfile, os
+from absl.testing import absltest
+import export
+from export import train_and_export
 
-FLAGS = None
+TMPDIR = tempfile.mkdtemp()
+model = None
+
+def load_model(path):
+  global model
+  if not model:
+    model = hub.load(path)
+  return model
 
 class TFHubMNISTTest(tf.test.TestCase):
   def setUp(self):
-    kwargs = {}
-    if FLAGS.data_dir:
-      kwargs["data_dir"] = FLAGS.data_dir
-    self.dataset = tfds.load("mnist",
-                             split="test",
-                             **kwargs).shuffle(FLAGS.buffer_size,
-                                               reshuffle_each_iteration=True).batch(FLAGS.batch_size)
-    self.batch = FLAGS.batch_size
-    self.accuracy_flag = FLAGS.check_accuracy
-    self.model = hub.load(FLAGS.exported_path)
+    file_ = tf.gather(tf.io.matching_files("test_data/*.jpg"), 0)
+    self.test_image = tf.expand_dims(tf.image.decode_jpeg(tf.io.read_file(file_)), 0)
+    self.test_label = int(str(file_.numpy()).split("/")[-1].split(".jpg")[0])
+  def test_model_exporting(self):
+    mocked_dict = export.sys.__dict__.copy()
+    devnull = open(os.devnull, "w")
+    mocked_dict["stdout"] = devnull
+    absltest.mock.patch.dict(
+    export.sys.__dict__,
+    mocked_dict,
+    clear=True).start()
+    train_and_export(epoch=1, export_path="%s/model/1" % TMPDIR)
+    self.assertTrue(os.listdir(TMPDIR))
 
-  def checkModelLoaded(self):
-    output_ = self.model.call(
-        tf.zeros([self.batch, 28, 28, 1], dtype=tf.uint8).numpy())
-    self.assertEqual(output_.shape, [self.batch, 10])
+  def test_model_loadable(self):
+    if not os.path.exists("%s/model/1" % TMPDIR):
+      self.test_model_exporting()
+    model = load_model("%s/model/1" % TMPDIR)
+    output_ = model.call(
+        tf.zeros([1, 28, 28, 1], dtype=tf.uint8).numpy())
+    self.assertEqual(output_.shape, [1, 10])
 
-  def checkModelWorking(self):
-    sample = next(iter(self.dataset.take(1)))
-    prediction = self.model.call(sample['image'])
-    self.assertEqual(tf.argmax(prediction).numpy(), sample['label'])
-
-  def checkAccuracy(self):
-    accuracy = 0
-    for data in self.dataset:
-      preds = self.model.call(data['image'])
-      preds = tf.argmax(preds, axis=1).numpy()
-      accuracy += sum(preds == data['label'].numpy()) / self.batch
-    print("Accuracy: %.2f" % (accuracy * 100))
-
+  def test_model_working(self):
+    if not os.path.exists(os.path.join(TMPDIR, "model/1")):
+      self.test_model_exporting()
+    model = load_model("%s/model/1" % TMPDIR)
+    prediction = model.call(self.test_image)
+    self.assertEqual(tf.argmax(tf.squeeze(prediction)).numpy(), self.test_label)
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--exported_path", type=str, default="/tmp/tfhub_modules/mnist/digits/1",help="Path to exported Module")
-  parser.add_argument("--data_dir", type=str, default=None, help="Path to TFDS Dataset Directory")
-  parser.add_argument("--check_accuracy", type=bool, default=False,help="Path to TFDS Dataset Directory")
-  parser.add_argument("--batch_size", type=int, default=32, help="Batch size for checking accuracy")
-  parser.add_argument("--buffer_size", type=int, default=1000, help="Buffer size used for Data Shuffling")
-  FLAGS, unk = parser.parse_known_args()
   tf.test.main()
+
