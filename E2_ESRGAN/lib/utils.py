5576f2cb65b13c99a73e80b538552b111d5fe3e8
captain-pool <rickdey1998@gmail.com>
2019-07-10 17:24:19 +0530

    used partial instead lambda

--- E2_ESRGAN/lib/utils.py
+++ E2_ESRGAN/lib/utils.py
@@ -165,7 +165,7 @@ class RDB(tf.keras.layers.Layer):
   def __init__(self, out_features=32, bias=True):
     super(RDB, self).__init__()
 
-    def _create_conv2d(): return tf.keras.layers.Conv2D(
+    _create_conv2d = partial(tf.keras.layers.Conv2D,
         out_features,
         kernel_size=[3, 3],
         strides=[1, 1], padding="same", use_bias=bias)
