6221307d2f1d511c909437aa5e64395b2110cd54
captain-pool <rickdey1998@gmail.com>
2019-07-18 13:10:24 +0530

    Moved Perceptual Loss Calculation to CPU

--- E2_ESRGAN/main.py
+++ E2_ESRGAN/main.py
@@ -49,7 +49,6 @@ def main(**kwargs):
   Stats = settings.Stats(os.path.join(sett.path, "stats.yaml"))
   summary_writer = tf.summary.create_file_writer(kwargs["log_dir"])
   profiler.start_profiler_server(6009)
-
   generator = model.RRDBNet(out_channel=3)
   discriminator = model.VGGArch()
 
