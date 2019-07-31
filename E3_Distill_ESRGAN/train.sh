8afd3917a4c260955c62105c84cdb3f747c20c14
Adrish Dey <rickdey1998@gmail.com>
2019-07-31 18:54:05 +0530

    Update train.sh

--- E3_Distill_ESRGAN/train.sh
+++ E3_Distill_ESRGAN/train.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+# $BASE contains the path to the cloud bucket.
+# $TPU_NAME contains the name of the cloud bucket.
 sudo tensorboard --logdir $BASE/logdir --port 80 &> $HOME/logdir/access.log &
-python3 main.py --logdir $BASE/logdir \
+python3 main.py --logdir $BASE/distillation/logdir \
 	--datadir $BASE/datadir/coco2014_esrgan\
 	--tpu $TPU_NAME \
 	--modeldir $BASE/distillation/modeldir \
