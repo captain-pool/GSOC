e1de5b78fb099a45678b7e506444bcb821debe9e
captain-pool <rickdey1998@gmail.com>
2019-08-21 15:51:27 +0530

    Linted Code. Added Docstrings

--- E1_TPU_Sample/image_retraining_tpu_strategy.py
+++ E1_TPU_Sample/image_retraining_tpu_strategy.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
+""" TensorFlow Sample for running TPU Training """
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -23,40 +23,45 @@ import argparse
 import tensorflow.compat.v2 as tf
 import tensorflow_hub as hub
 import tensorflow_datasets as tfds
+
 tf.enable_v2_behavior()
 os.environ["TFHUB_DOWNLOAD_PROGRESS"] = "True"
 
 PRETRAINED_KERAS_LAYER = "https://tfhub.dev/google/tf2-preview/inception_v3/feature_vector/4"
-BATCH_SIZE = 32 # In case of TPU, Must be a multiple of 8
+BATCH_SIZE = 32  # In case of TPU, Must be a multiple of 8
+
 
 class SingleDeviceStrategy(object):
   """ Dummy Class to mimic tf.distribute.Strategy for Single Devices """
+
   def __enter__(self, *args, **kwargs):
     pass
-  
+
   def __exit__(self, *args, **kwargs):
     pass
-  
+
   def scope(self):
     return self
-  
-  def experimental_distribute_dataset(self, dataset, *args, **kwargs):
+
+  def experimental_distribute_dataset(self, dataset):
     return dataset
-  
+
   def experimental_run_v2(self, func, args, kwargs):
     return func(*args, **kwargs)
-  
-  def reduce(self, reduction_type, distributed_data, axis):
+
+  def reduce(self, reduction_type, distributed_data, axis): #pylint: disable=unused-argument
     return distributed_data
 
+
 class Model(tf.keras.layers.Model):
-	def __init__(self, num_classes):
-		self._pretrained_layer = hub.KerasLayer(
+  """ Keras Model class for Image Retraining """
+  def __init__(self, num_classes):
+    self._pretrained_layer = hub.KerasLayer(
         PRETRAINED_KERAS_LAYER,
         output_shape=[2048],
         trainable=False)
     self._dense_1 = tf.keras.layers.Dense(num_classes, activation="sigmoid")
- 
+
   @tf.function(
       input_signature=[
           tf.TensorSpec(
@@ -79,7 +84,16 @@ def connect_to_tpu(tpu=None):
     return strategy, "/task:1"
   return SingleDeviceStrategy(), ""
 
+
 def load_dataset(name, datadir, batch_size=32, shuffle=None):
+  """
+    Loads and preprocesses dataset from TensorFlow dataset.
+    Args:
+      name: Name of the dataset to load
+      datadir: Directory to the dataset in.
+      batch_size: size of each minibatch. Must be a multiple of 8.
+      shuffle: size of shuffle buffer to use. Not shuffled if set to None.
+  """
   dataset, info = tfds.load(
       name,
       try_gcs=True,
@@ -95,32 +109,48 @@ def load_dataset(name, datadir, batch_size=32, shuffle=None):
     image -= 1.
     label = tf.one_hot(label, num_classes)
     return image, label
-  
+
   options = tf.data.Options()
   if not hasattr(tf.data.Options.auto_shard):
     options.experimental_distribute.auto_shard = False
   else:
     options.auto_shard = False
 
-  dataset = (dataset.map(
-      _scale_fn,
-      num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    .with_options(options)
-    .batch(batch_size, drop_remainder=True))
+  dataset = (
+      dataset.map(
+          _scale_fn,
+          num_parallel_calls=tf.data.experimental.AUTOTUNE)
+      .with_options(options)
+      .batch(batch_size, drop_remainder=True))
   if shuffle:
     dataset = dataset.shuffle(shuffle, reshuffle_each_iteration=True)
   return dataset.repeat(), num_classes
 
+
 def train_and_export(**kwargs):
+  """
+    Trains the model and exports as SavedModel.
+    Args:
+      tpu: Name or GRPC address of the TPU to use.
+      logdir: Path to a bucket or directory to store TensorBoard logs.
+      modeldir: Path to a bucket or directory to store the model.
+      datadir: Path to store the downloaded datasets to.
+      dataset: Name of the dataset to load from TensorFlow Datasets.
+      num_steps: Number of steps to train the model for.
+  """
   if kwargs["tpu"]:
     # For TPU Training the Files must be stored in
     # Cloud Buckets for the TPU to access
-    assert kwargs["logdir"].startswith("gs://")
-    assert kwargs["modeldir"].startswith("gs://")
+    if not kwargs["logdir"].startswith("gs://"):
+      raise ValueError("To train on TPU. `logdir` must be cloud bucket")
+    if not kwargs["modeldir"].startswith("gs://"):
+      raise ValueError("To train on TPU. `modeldir` must be cloud bucket")
     if kwargs["datadir"]:
-      assert kwargs["datadir"].startswith("gs://")
+      if not kwargs["datadir"].startswith("gs://"):
+        raise ValueError("To train on TPU. `datadir` must be a cloud bucket")
 
-  os.environ["TFHUB_CACHE_DIR"] = os.path.join(kwargs["modeldir"], "tfhub_cache")
+  os.environ["TFHUB_CACHE_DIR"] = os.path.join(
+      kwargs["modeldir"], "tfhub_cache")
 
   strategy, device = connect_to_tpu(kwargs["tpu"])
 
@@ -135,17 +165,18 @@ def train_and_export(**kwargs):
     model = Model(num_classes)
     loss_metric = tf.keras.metrics.Mean()
     optimizer = tf.keras.optimizers.Adam()
+
     def distributed_step(images, labels):
       with tf.GradientTape() as tape:
         predictions = model.unsigned_call(images)
         loss = tf.nn.sigmoid_cross_entropy_with_logits(labels, predictions)
         loss_metric(loss)
-        loss = loss * (1.0 /  BATCH_SIZE)
+        loss = loss * (1.0 / BATCH_SIZE)
       gradient = tape.gradient(loss, model.trainable_variables)
       train_op = optimizer.apply_gradients(gradient, model.trainable_variables)
       with tf.control_dependencies([train_op]):
         return tf.cast(optimizer.iterations, tf.float32)
-    
+
     @tf.function
     def train_step(image, label):
       distributed_metric = strategy.experimental_run_v2(
@@ -153,7 +184,7 @@ def train_and_export(**kwargs):
       step = strategy.reduce(
           tf.distribute.ReduceOp.MEAN, distributed_metric, axis=None)
       return step
-    
+
     while True:
       image, label = next(dataset)
       step = train_step(image, label)
@@ -167,15 +198,39 @@ def train_and_export(**kwargs):
   logging.info("Exporting Saved Model")
   tf.saved_model.save(model, os.path.join(kwargs["modeldir"], "model"))
 
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
-  parser.add_argument("dataset", default=None, help="Name of the Dataset to use")
-  parser.add_argument("datadir", default=None, help="Directory to store the downloaded Dataset")
-  parser.add_argument("modeldir", default=None, help="Directory to store the SavedModel to")
-  parser.add_argument("logdir",default= None, help="Directory to store the Tensorboard logs")
-  parser.add_argument("tpu",  default=None, help="name or GRPC address of the TPU")
-  parser.add_argument("num_steps", default=1000, help="Number of Steps to train the model for")
-  parser.add_argument("--verbose", "-v", default=0, action="count", help="increase verbosity. multiple tags to increase more")
+  parser.add_argument(
+      "dataset",
+      default=None,
+      help="Name of the Dataset to use")
+  parser.add_argument(
+      "datadir",
+      default=None,
+      help="Directory to store the downloaded Dataset")
+  parser.add_argument(
+      "modeldir",
+      default=None,
+      help="Directory to store the SavedModel to")
+  parser.add_argument(
+      "logdir",
+      default=None,
+      help="Directory to store the Tensorboard logs")
+  parser.add_argument(
+      "tpu",
+      default=None,
+      help="name or GRPC address of the TPU")
+  parser.add_argument(
+      "num_steps",
+      default=1000,
+      help="Number of Steps to train the model for")
+  parser.add_argument(
+      "--verbose",
+      "-v",
+      default=0,
+      action="count",
+      help="increase verbosity. multiple tags to increase more")
   flags, unknown = parser.parse_known_args()
   log_levels = [logging.FATAL, logging.WARNING, logging.INFO, logging.DEBUG]
   log_level = log_levels[min(flags.verbose, len(log_levels) - 1)]
